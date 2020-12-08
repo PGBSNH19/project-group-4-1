9acee7bfeaaca08c0bceed97c4ccb58cac28cebd
Bluemace <36447393+Bluemace@users.noreply.github.com>
2020-12-08 15:30:03 +0100

    first version, currently not working
    First version done in mob-programming, currently not working.

--- Source/API/GraphQL/GraphQLQueries/AppQuery.cs
+++ Source/API/GraphQL/GraphQLQueries/AppQuery.cs
@@ -1,9 +1,5 @@
 ﻿using API.GraphQLTypes;
 using GraphQL.Types;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace API.GraphQL.GraphQLQueries
 {
---
+++ Source/BlazorApp-Frontend/Data/MessageService.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Net.Http;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+namespace BlazorApp_Frontend.Data
+{
+    public class MessageService
+    {
+        public HttpClient Client { get; }
+
+        public MessageService(HttpClient client)
+        {
+            client.BaseAddress = new Uri("http://localhost:5000/");
+            Client = client;
+        }
+        public async Task<string> GetMessage()
+        {
+            var response = await Client.GetAsync("graphql?query={messages{id,text}}");
+            using var responseStream = await response.Content.ReadAsStreamAsync();
+            var messageList = await JsonSerializer.DeserializeAsync<Message>(responseStream);
+
+            string message = messageList.Text;
+
+
+            return message;
+        }
+
+
+        public int id;
+
+        public string text;
+    }
+
+    public class Message
+    {
+        [JsonPropertyName("id")]
+        public int Id { get; set; }
+        [JsonPropertyName("text")]
+        public string Text { get; set; }
+    }
+}
