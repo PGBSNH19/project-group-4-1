ffcd8eea1f3ff568a5c2d34410426a8a3bd100f4
Bluemace <36447393+Bluemace@users.noreply.github.com>
2020-12-18 22:10:03 +0100

    Attempt to get Data from api
    First attempt to get Data from the API, will test in Test env.

---
+++ Source/BlazorApp-Frontend/Data/Marketplace.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace BlazorApp_Frontend.Data
+{
+    public class Marketplace
+    {
+        public int MarketplaceID { get; set; }
+
+        public string Name { get; set; }
+
+        public string Location { get; set; }
+
+        public DateTime StartDateTime { get; set; }
+
+        public DateTime EndDateTime { get; set; }
+
+        //public ICollection<MarketplaceSeller> MarketplaceSellers { get; set; }
+    }
+}
