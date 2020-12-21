424e88d3df9f484c714e9f9483ebe90745a6f44e
oskarmorell <oskarmorell1998@hotmail.se>
2020-12-21 10:59:23 +0100

    Marketplace view minor changes

---
+++ Source/BlazorApp-Frontend/Services/MarketplaceRepository.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using BlazorApp_Frontend.Data;
+using Microsoft.AspNetCore.Components;
+
+namespace BlazorApp_Frontend.Services
+{
+    public class MarketplaceRepository
+    {
+        public HttpClient http { get; }
+        public MarketplaceRepository(HttpClient client)
+        {
+            // client.BaseAddress = new Uri("https://nearbyproduceapiTest.azurewebsites.net");
+            http = client;
+        }
+
+        public async Task<List<Marketplace>> GetAllMarketplaces()
+        {
+            var marketplaces = await http.GetJsonAsync<List<Marketplace>>("https://nearbyproduceapitest.azurewebsites.net/api/v1.0/Marketplace/GetMarketplaces");
+            return marketplaces;
+        }
+    }
+}
