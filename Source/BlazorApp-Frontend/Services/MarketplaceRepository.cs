a75f3c906066c0979779fbc1aff7bab7d654d6d0
Bluemace <36447393+Bluemace@users.noreply.github.com>
2020-12-21 10:51:04 +0100

    Created API and view connection
    Created services in the frontend for all Get and post calls from the api.
    [G4-24] [G4-21]

---
+++ Source/BlazorApp-Frontend/Services/MarketplaceRepository.cs
@@ -0,0 +1,41 @@
+﻿using BlazorApp_Frontend.Data;
+using Microsoft.AspNetCore.Components;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorApp_Frontend.Services
+{
+    public class MarketplaceRepository
+    {
+        public HttpClient http { get; }
+        public MarketplaceRepository(HttpClient client)
+        {
+            client.BaseAddress = new Uri("https://nearbyproduceapiTest.azurewebsites.net");
+            http = client;
+        }
+
+        public async Task<List<Marketplace>> GetMarketplaces()
+        {
+            var marketplaces = await http.GetJsonAsync<List<Marketplace>>(http.BaseAddress + "/api/v1.0/Marketplace/GetMarketplaces");
+            return marketplaces;
+        }
+
+        public async Task<List<Marketplace>> GetMarketplaceById(int id)
+        {
+            var marketplace = await http.GetJsonAsync<List<Marketplace>>(http.BaseAddress + $"/api/v1.0/Marketplace/GetMarketplace/{id}");
+            return marketplace;
+        }
+        public async Task<Marketplace> PostMarketplace(Marketplace marketplaceToCreate)
+        {
+            var data = new StringContent(JsonConvert.SerializeObject(marketplaceToCreate), Encoding.UTF8, "application/json");
+
+            var marketplace = await http.PostJsonAsync<Marketplace>(http.BaseAddress + "api/v1.0/Marketplace", data);
+            return marketplace;
+        }
+    }
+}
+
