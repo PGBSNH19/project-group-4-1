51519687b34ebf0b0c1daed97c3882e4d505669d
Bluemace <36447393+Bluemace@users.noreply.github.com>
2020-12-22 14:33:21 +0100

    Small fixes
    Added json properties in models and included more data in calls from api.

--- Source/BlazorApp-Frontend/Data/Marketplace.cs
+++ Source/BlazorApp-Frontend/Data/Marketplace.cs
@@ -1,19 +1,32 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
 
 namespace BlazorApp_Frontend.Data
 {
     public class Marketplace
     {
+        [JsonProperty("marketplaceid")]
         public int MarketplaceID { get; set; }
 
+
+        [JsonProperty("name")]
         public string Name { get; set; }
 
+
+        [JsonProperty("location")]
         public string Location { get; set; }
 
+
+        [JsonProperty("startdatetime")]
         public DateTime StartDateTime { get; set; }
 
+
+        [JsonProperty("enddatetime")]
         public DateTime EndDateTime { get; set; }
 
-        //public ICollection<MarketplaceSeller> MarketplaceSellers { get; set; }
+
+        [JsonProperty("marketplacesellers")]
+        public ICollection<MarketplaceSeller> MarketplaceSellers { get; set; }
     }
 }
--- Source/BlazorApp-Frontend/Data/UserProduct.cs
+++ Source/BlazorApp-Frontend/Data/UserProduct.cs
@@ -1,10 +1,19 @@
-﻿namespace BlazorApp_Frontend.Data
+﻿using Newtonsoft.Json;
+
+namespace BlazorApp_Frontend.Data
 {
     public class UserProduct
     {
+        [JsonProperty("userid")]
         public int UserID { get; set; }
+
+        [JsonProperty("user")]
         public User user { get; set; }
+
+        [JsonProperty("productid")]
         public int ProductID { get; set; }
+
+        [JsonProperty("product")]
         public Product product { get; set; }
     }
 }
