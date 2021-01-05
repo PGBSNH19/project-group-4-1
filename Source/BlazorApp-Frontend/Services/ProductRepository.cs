51c0c4f25d411fd9cc3b1cda87437714dbd984bc
Bluemace <36447393+Bluemace@users.noreply.github.com>
2021-01-05 16:15:35 +0100

    Updated address to api

--- Source/BlazorApp-Frontend/Services/ProductRepository.cs
+++ Source/BlazorApp-Frontend/Services/ProductRepository.cs
@@ -34,7 +34,7 @@ namespace BlazorApp_Frontend.Services
         {
             var data = new StringContent(JsonConvert.SerializeObject(productToCreate), Encoding.UTF8, "application/json");
 
-            var product = await http.PostJsonAsync<Product>("/api/v1.0/Product", data);
+            var product = await http.PostJsonAsync<Product>(http.BaseAddress + "/api/v1.0/Product", data);
             return product;
         }
 
@@ -43,7 +43,7 @@ namespace BlazorApp_Frontend.Services
 
 
             var data = new StringContent(JsonConvert.SerializeObject(productToChange), Encoding.UTF8, "application/json");
-            var response = await http.PutAsync($"https://localhost:5002/api/v1.0/Product/{productToChange.ProductID}", data);
+            var response = await http.PutAsync(http.BaseAddress + $"/api/v1.0/Product/{productToChange.ProductID}", data);
 
             return response;
         }
