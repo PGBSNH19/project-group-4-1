0fc51349519c04ae2d96c18bfff595a7611dcf48
Bluemace <36447393+Bluemace@users.noreply.github.com>
2021-01-11 12:07:33 +0100

    fixed merge conflict

--- Source/BlazorApp-Frontend/Data/SellerPage.cs
+++ Source/BlazorApp-Frontend/Data/SellerPage.cs
@@ -5,16 +5,22 @@ namespace BlazorApp_Frontend.Data
 {
     public class SellerPage
     {
+        [JsonProperty("sellerpageid")]
         public int SellerPageID { get; set; }
 
+        [JsonProperty("name")]
         public string Name { get; set; }
 
+        [JsonProperty("selleruserid")]
         public int SellerUserID { get; set; }
 
+        [JsonProperty("seller")]
         public User Seller { get; set; }
-        public string Description { get; set; }
 
         [JsonProperty("sellerpageproducts")]
         public ICollection<SellerPageProduct> SellerPageProducts { get; set; }
+
+        [JsonProperty("description")]
+        public string Description { get; set; }
     }
 }
