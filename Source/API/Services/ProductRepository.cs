e967a4053cf77f8081eb9ba467fa8a06c7e8ec2c
robotdostopif <aron.cederlund@outlook.com>
2021-01-14 14:24:56 +0100

    Refacted Product Controller and Repo

--- Source/API/Services/ProductRepository.cs
+++ Source/API/Services/ProductRepository.cs
@@ -21,8 +21,8 @@ namespace API.Services
 
         public async Task<Product> GetProductById(int id)
         {
-            IQueryable<Product> query = _context.Products.Where(x => x.ProductID == id);
-            return await query.FirstOrDefaultAsync();
+            var query = await _context.Products.FindAsync(id);
+            return query;
         }
     }
 }
